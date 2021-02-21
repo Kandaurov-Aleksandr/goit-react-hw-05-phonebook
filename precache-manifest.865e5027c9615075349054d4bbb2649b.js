6f33100abaa3306345159caf1515803843fdc01c
Kandaurov-Aleksandr <kandayrov1990@gmail.com>
2021-02-21 20:47:35 +0200

    Updates

---
+++ precache-manifest.865e5027c9615075349054d4bbb2649b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c48cfe6d63b5f65b5b58",
+    "url": "/goit-react-hw-05-phonebook/static/css/main.97484c3e.chunk.css"
+  },
+  {
+    "revision": "c48cfe6d63b5f65b5b58",
+    "url": "/goit-react-hw-05-phonebook/static/js/main.c48cfe6d.chunk.js"
+  },
+  {
+    "revision": "173b568d2a064c402ced",
+    "url": "/goit-react-hw-05-phonebook/static/js/1.173b568d.chunk.js"
+  },
+  {
+    "revision": "5f60b32f1447f176434d",
+    "url": "/goit-react-hw-05-phonebook/static/js/runtime~main.5f60b32f.js"
+  },
+  {
+    "revision": "f30287d4c17505830a3dcc3f039704d3",
+    "url": "/goit-react-hw-05-phonebook/index.html"
+  }
+];
